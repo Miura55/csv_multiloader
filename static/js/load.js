cdd85b98d47d4f382d4f57d3dae669394d9652af
Miura55 <acordion.piano@gmail.com>
2019-09-13 17:59:34 +0900

    Add debug

--- static/js/load.js
+++ static/js/load.js
@@ -2,6 +2,6 @@ $(document).on('change', ':file', function() {
     var input = $(this),
     numFiles = input.get(0).files ? input.get(0).files.length : 1,
     label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    // label = input.val();
+    console.log(label);
     input.parent().parent().next(':text').val(label);
 });
