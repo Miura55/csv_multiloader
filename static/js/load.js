a310cd7f9a989bbe7ef1d1e239e69e2e4661bde1
Miura55 <acordion.piano@gmail.com>
2019-09-14 15:50:05 +0900

    Updated UI

--- static/js/load.js
+++ static/js/load.js
@@ -2,6 +2,14 @@ $(document).on('change', ':file', function() {
     var input = $(this),
     numFiles = input.get(0).files ? input.get(0).files.length : 1,
     label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    console.log(label);
-    input.parent().parent().next(':text').val(label);
+    var filelist = "";
+    for (i = 0; i < input.get(0).files.length; i++) {
+        if (i != 0){
+            filelist += ", " + input.get(0).files[i].name;
+        }else{
+            filelist += input.get(0).files[0].name;
+        }        
+    }
+    console.log(numFiles);
+    input.parent().parent().next(':text').val(filelist);
 });
